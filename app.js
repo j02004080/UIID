79ab04522f49df8e881cb0a9ceb6b89287ba55b4
cloud <j02004080@gmail.com>
2016-05-04 16:51:35 +0800

    0504

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+var fs = require('fs');
+
+app.listen(8109, function(){
+  console.log('server run');
+});
+
+app.use(express.static('public/'));
+app.get('/Home',function(req,res){
+  res.sendFile(path.join(__dirname+ '/index.html'));
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+
+
---
+++ public/Home/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#button").click(function(){
+		$("#house").animate({
+			width: "100%", 
+			margin: "0 0", 
+			bottom: "0"
+			
+		}, 3000);
+		
+	})
+});
