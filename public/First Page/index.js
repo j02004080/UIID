ce61ce67e1fa74666a98168af822fcc9ff331fa1
Ycblue <yuchialan@gmail.com>
2016-05-11 15:54:36 +0800

    modified index-mobile

--- public/First Page/index.js
+++ public/First Page/index.js
@@ -1,11 +1,19 @@
-$(document).ready(function(){
-	$("#button").click(function(){
-		$("#house").animate({
-			width: "100%", 
-			margin: "0 0", 
-			bottom: "0"
+$(doc   ument).ready(function(){
+	$("#button").click(function(e){
+    e.preventDefault();
+    var goTo = this.getAttribute("href");
+
+    $("#house").animate({
+			width: "200%", 
+			margin: "0 -50%", 
+			top: "-50vh",
+      bottom: "0",
+
 			
 		}, 3000);
 		$("#man").fadeOut(3000);
-	})
+	  setTimeout(function(){
+      window.location=goTo;
+    }, 3000);
+  })
 });
